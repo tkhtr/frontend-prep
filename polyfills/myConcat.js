a0d0bec81e940b08b2a9eacec52860a793091736
Tokhtar Aubakirov <tokhtar.aubakirov@ffins.kz>
2025-11-15 13:03:43 +0500

    concat function

--- polyfills/myConcat.js
+++ polyfills/myConcat.js
@@ -10,6 +10,7 @@ Array.prototype.myConcat = function (...items) {
     if(!Array.isArray(val)) {
       result.push(val);
     } else {
+      //result.push.apply(result, val);
       //Array.prototype.push.apply(result, val);
       result = [...result, ...val];
     }
